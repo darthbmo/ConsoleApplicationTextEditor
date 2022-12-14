06aabf2bcd57b1edea3efa2bff86803d3f678e3f
darthbmo <connectionreverse@gmail.com>
2022-12-14 10:23:36 -0300

    Changed the idiom of writing of project to english US

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Menu();
+
+static void Menu(){
+    Console.Clear();
+
+    Console.WriteLine("What do you want to do?");
+    
+    Console.WriteLine("1 - Open text file existing.");
+
+    Console.WriteLine("2 - Create a new text file.");
+
+    string? option = Console.ReadLine();
+}
